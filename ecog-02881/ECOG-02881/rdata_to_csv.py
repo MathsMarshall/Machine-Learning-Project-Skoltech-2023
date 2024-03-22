4cb45114afd7995a8f855e373006510813e287d4
Holdinzz <125668733+Holdinzz@users.noreply.github.com>
2024-03-22 20:23:15 +0300

    Delete ecog-02881 directory

--- ecog-02881/ECOG-02881/rdata_to_csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import pyreadr
-
-
-result = pyreadr.read_r("Appendix_6_Box_4_DATA_NorthAmerica_DougFir.rdata") # Read .rdata file
-df = result['tr'] #convert R data to pandas DataFrame
-df.to_csv("Box4_data.csv",index=False)
